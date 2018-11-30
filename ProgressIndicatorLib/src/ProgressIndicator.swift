9037e1b475f36f64b3cda2d7e7ea046609a0cea9
Eonist <30n1st@gmail.com>
2018-11-30 09:06:36 +0100

    Move background to init

--- ProgressIndicatorLib/src/ProgressIndicator.swift
+++ ProgressIndicatorLib/src/ProgressIndicator.swift
@@ -15,12 +15,19 @@ open class ProgressIndicator:UIView {
    open var lineStyle:LineStyle = ProgressIndicator.defaultStyle
    open lazy var animator:TimeAnimator = createAnimator()//   lazy var animator:Animator = {LoopingAnimator(AnimProxy.shared,Int.max,1,0,1,self.progress,Linear.ease)}()
    open var revealProgress:CGFloat = 0
+   public override init(frame: CGRect) {
+      super.init(frame: frame)
+      self.backgroundColor = .lightGray
+   }
+   
+   required public init?(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+   }
    /**
     * Note: We relay on getting .frame in the subsequent called methods, as such we need to be mindful when we access it, as its only available after AutoLayout has done its bidding.
     */
    override open func layoutSubviews() {
       super.layoutSubviews()
-      self.backgroundColor = .lightGray
       createIndicators()
    }
 }
